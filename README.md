4946c22a0e1a51a7ebbc47a46fb34f050fb47532
Erik Rigtorp <erik@rigtorp.se>
2016-01-04 21:57:17 -0500

    Add benchmark for invokation overhead

--- README.md
+++ README.md
@@ -9,9 +9,18 @@ for example *std::string*.
 
 ## Benchmark
 
-*Function.h* is almost 3 times faster than *std::function*.
+*Function.h* is quite a lot faster than *std::function* to
+ construct. Invocation overhead is the same for both.
+
+Compiled with `gcc -O3 -fno-devirtualize` (gcc 5.2).
 
 ```
-std::function: 87ns/op
-Function:      29ns/op
+construction overhead
+  std::function: 42ns/op
+  Function:      4ns/op
+  
+invokation overhead:
+  std::function: 2ns/op
+  Function:      2ns/op
+  virtual:       2ns/op
 ```
--- benchmark.cpp
+++ benchmark.cpp
@@ -5,13 +5,20 @@
 int main(int argc, char *argv[]) {
   constexpr size_t count = 100000000;
 
-  volatile size_t state = 0;
-
   using namespace std::chrono;
+
+  volatile size_t state = 0;
+  struct State {
+    size_t a, b, c;
+  };
+  State state2 = {0, 0, 0};
+
+  std::cout << "construction overhead" << std::endl;
+
   {
     auto start = high_resolution_clock::now();
     for (size_t i = 0; i < count; ++i) {
-      std::function<void()> stdfun([&state, i]() { state = i; });
+      std::function<void()> stdfun = [&state, state2, i]() { state = i; };
       stdfun();
     }
     auto stop = high_resolution_clock::now();
@@ -24,12 +31,62 @@ int main(int argc, char *argv[]) {
   {
     auto start = high_resolution_clock::now();
     for (size_t i = 0; i < count; ++i) {
-      Function<void()> fun([&state, i]() { state = i; });
+      Function<void()> fun = [&state, state2, i]() { state = i; };
       fun();
     }
     auto stop = high_resolution_clock::now();
     auto duration = stop - start;
     std::cout << "Function: "
+              << duration_cast<nanoseconds>(duration).count() / count << "ns/op"
+              << std::endl;
+  }
+
+  std::cout << "invokation overhead" << std::endl;
+
+  {
+    std::function<void(size_t)> stdfun([&state](size_t i) { state = i; });
+    auto start = high_resolution_clock::now();
+    for (size_t i = 0; i < count; ++i) {
+      stdfun(i);
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = stop - start;
+    std::cout << "std::function: "
+              << duration_cast<nanoseconds>(duration).count() / count << "ns/op"
+              << std::endl;
+  }
+
+  {
+    Function<void(size_t)> fun([&state](size_t i) { state = i; });
+    auto start = high_resolution_clock::now();
+    for (size_t i = 0; i < count; ++i) {
+      fun(i);
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = stop - start;
+    std::cout << "Function: "
+              << duration_cast<nanoseconds>(duration).count() / count << "ns/op"
+              << std::endl;
+  }
+
+  {
+    // Must compile this with -fno-devirtualize to prevent inlining
+    struct Fun {
+      virtual void fun(size_t) = 0;
+    };
+    struct Impl : public Fun {
+      void fun(size_t i) override { state = i; };
+      volatile size_t state = 0;
+    };
+    Impl impl;
+    auto start = high_resolution_clock::now();
+    for (size_t i = 0; i < count; ++i) {
+      Fun *fun = &impl;
+      fun->fun(i);
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = stop - start;
+    std::cout << "virtual: "
               << duration_cast<nanoseconds>(duration).count() / count << "ns/op"
               << std::endl;
   }
