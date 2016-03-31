e58c3c90d3fa2969a7157091fad604df92ff9dcd
Pavel Medvedev <pmedvedev@gmail.com>
2016-04-01 02:00:01 +0300

    Add MaxSize template parameter to control size of Function
    MaxSize is used to calculate data storage size with according
    to invoker and manager sizes.

--- example.cpp
+++ example.cpp
@@ -5,12 +5,13 @@ int main(int argc, char *argv[]) {
   int foo = 1;
   double bar = 3;
 
-  Function<int (std::ostream &)> f([=](std::ostream &os) {
+  Function<int (std::ostream &), 128> f([=](std::ostream &os) {
     os << "test " << foo << " " << bar << std::endl;
     return foo;
   });
 
   f(std::cout);
+  std::cout << "Size of Function: " << sizeof(f) << '\n';
 
   return 0;
 }
